fe3ddf165e257acbc0431beff530323fc9cd65b1
Kristofers Solo <dev@kristofers.xyz>
2025-07-07 22:16:24 +0300

    refactor(units): use derive macro

--- src/app/utils/netspeed.rs
+++ src/app/utils/netspeed.rs
@@ -1,8 +1,9 @@
-use super::unit::{Unit, UnitError};
-use crate::app::utils::unit::UnitDisplay;
+use super::unit::{Unit, UnitDisplay};
+use derive_macro::UnitConversions;
 use std::fmt::Display;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Default)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Default, UnitConversions)]
+#[error(UnitError)]
 pub struct NetSpeed(Unit);
 
 impl NetSpeed {
@@ -17,35 +18,3 @@ impl Display for NetSpeed {
         write!(f, "{}", UnitDisplay::new(&self.0, UNITS))
     }
 }
-
-macro_rules! impl_from_unsigned {
-    ($type:ty, $($t:ty), *) => {
-        $(
-            impl From<$t> for $type {
-                fn from(value: $t) -> Self {
-                    Self(Unit::from(value))
-                }
-            }
-        )*
-    };
-}
-
-macro_rules! impl_try_from_signed {
-    ($type:ty, $error:ty, $($t:ty), *) => {
-        $(
-            impl TryFrom<$t> for $type {
-                type Error = $error;
-
-                fn try_from(value: $t) -> Result<Self, Self::Error> {
-                    if value < 0 {
-                        return Err(UnitError::NegativeValue { value: value as i64 });
-                    }
-                    Ok(Self(Unit::try_from(value)?))
-                }
-            }
-        )*
-    };
-}
-
-impl_from_unsigned!(NetSpeed, u8, u16, u32, u64, usize);
-impl_try_from_signed!(NetSpeed, UnitError, i8, i16, i32, i64, isize);
